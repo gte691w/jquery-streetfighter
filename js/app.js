020ef89614e74a3a66408a6ba3ce8c5398728c02
Alex Soro <yasharsoroosh@yashars-air.lan>
2014-12-16 15:09:50 -0500

    Added the Ryu Cool image and programmed a function where if the x key is pressed Ryu transforms from the Ready position to the Cool postion.

---
+++ js/app.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+	$(".ryu").on('mouseenter', function(){
+		$(".ryu-still").hide();
+		$(".ryu-ready").show();
+	});
+
+	$(this).on('mouseleave', function() {
+      	$(".ryu-ready").hide();
+      	$(".ryu-still").show();
+		
+  	});
+
+  	$(this).on('mousedown', function(){
+  		playHadouken ();
+  		$(".ryu-ready").hide();
+  		$(".ryu-throwing").show();
+  		$('.hadouken').show();
+  		$('.hadouken').finish().show().animate({'left': '1006px'},500,
+  		  function() {
+    		$(this).hide();
+   		    $(this).css('left', '503px');
+ 		 }
+	);
+  	});
+
+  	$(this).on('mouseup', function(){
+  		$(".ryu-throwing").hide();
+  		$('.hadouken').hide();
+  		$(".ryu-ready").show();
+  	});
+
+  	$(document).on('keydown', function(event){
+  		if(event.keyCode === 88){
+  			$(".ryu-ready").hide();
+  			$(".ryu-cool").show();
+  		}
+
+
+  	});		
+
+  	$(document).on('keyup', function(event){
+  			$(".ryu-cool").hide();
+  			$(".ryu-ready").show();
+
+  	});		
+
+
+
+});
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
+
